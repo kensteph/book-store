ea96bb1c7c298121b8f0e7c648bc834dba09a2d5
Kender ROMAIN <kenderromain@gmail.com>
2023-02-27 12:20:55 -0400

    Define a slice of state for books.

---
+++ src/redux/features/books/booksSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// Initial state
+const initialState = [];
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {},
+});
